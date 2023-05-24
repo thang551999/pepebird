0004dbcb8e6c3f08ef778faefabcb9df90ee74c6
thangdp <thangdp@MacBook-Pro-cua-thangdp.local>
2023-05-24 20:23:28 +0700

    add log

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,25 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-0PSQKHK79S`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0PSQKHK79S', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
       <body>
         <Main />
         <NextScript />
