ae3528fa9e9e8189ac638b821ed040860bb40687
thangdp <thangdp@MacBook-Pro-cua-thangdp.local>
2023-05-27 01:20:03 +0700

    wwhitepaper

--- src/components/Pages/WhitePaper/index.tsx
+++ src/components/Pages/WhitePaper/index.tsx
@@ -1,6 +1,47 @@
 import ListImage from '../HomePage/ListImage';
 
 const WhitePaperPage = () => {
+  const RoadMapUpdateData = [
+    [
+      'Formation of the team',
+      'Creation of a website and social network presence',
+      'Fair launch on Pinksale',
+      'Demo of the Rent-To-Earn platform',
+      'Listing on CMC, CG, DEXTOOLS',
+      'Marketing efforts',
+    ],
+    [
+      'Update of the Whitepaper to version 2',
+      'Listing on a CEX (Centralized Exchange)',
+      'Commencement of RTE development',
+      'Targeted marketing campaigns in various markets',
+      'Introduction of stage 1 of the RTE program',
+    ],
+    [
+      'Wide-scale application of the platform',
+      'Introduction of stage 2 of the RTE program',
+      'Introduction of mini-games and NFTs',
+      'Roadmap update',
+    ],
+  ];
+
+  const tokennomicsText = [
+    'No taxes, contract renouncement, and audit',
+    'Airdrop: 2%',
+    'Marketing: 2%',
+    'Ecosystem: 6.9%',
+    'Presale and liquidity: 81.1%',
+    'Burn: 8%',
+  ];
+  const SOCIAL = [
+    'Contract: https://bscscan.com/token/0x091d471d8410cb14451a45f406922f271e356283',
+    'Telegram: https://t.me/bluebirdpepeofficial',
+    'Annoucement: https://t.me/bluebirdpepeannoucement',
+    'Twitter: https://twitter.com/BlueBirdPepe',
+    'Website: https://www.bluebirdpepe.com/',
+    'Contact: bluebirdpepeofficial@gmail.com',
+  ];
+
   return (
     <>
       <header>
@@ -19,16 +60,60 @@ const WhitePaperPage = () => {
       </div>
 
       <div className='section-description'>
-        <h2>I. INTRODUCTION</h2>
+        <h2>II. RENT TO EARN</h2>
 
         <p>
-          Blue Bird Pepe (BBPEPE) represents the fusion of the legendary Pepe meme and the symbol of free speech and fun
-          on Twitter. However, BBPEPE is not just another memecoin; it incorporates utility within the meme itself. With
-          a team of experienced individuals, BBPEPE aims to achieve the seemingly impossible.
+          BBPEPE introduces its most promising feature: Rent-To-Earn (RTE). Token holders can own billboards on our
+          platform in the form of NFTs and rent them out to individuals seeking to advertise. Profits from these rentals
+          will be shared between NFT holders and the platform, based on a predetermined percentage. Engaging mini-games
+          will be introduced to encourage participation among BBPEPE token holders. Our long-term vision involves
+          establishing a centralized platform where NFTs and token holders can earn from multiple income sources.
         </p>
       </div>
 
       <ListImage />
+
+      <div className='section-description' style={{ color: 'white' }}>
+        <h2>III. ROADMAP</h2>
+        {RoadMapUpdateData.map((e, i) => (
+          // eslint-disable-next-line react/jsx-key
+          <div style={{ marginTop: 16 }}>
+            <h3> Phase {i + 1}:</h3>
+            <ul style={{ listStyle: 'inherit', paddingLeft: 15 }}>
+              {e.map((item, index) => (
+                <li style={{ marginBlock: 8 }} key={index}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div className='section-description' style={{ color: 'white' }}>
+        <h2>III. TOKENOMICS</h2>
+        <div style={{ marginTop: 16 }}>
+          <ul style={{ listStyle: 'inherit', paddingLeft: 15 }}>
+            {tokennomicsText.map((item, index) => (
+              <li style={{ marginBlock: 8 }} key={index}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className='section-description' style={{ marginBottom: 134, color: 'white' }}>
+        <h2>IV. SOCIAL NETWORK</h2>
+        <div style={{ marginTop: 16 }}>
+          <ul style={{ listStyle: 'inherit', paddingLeft: 15 }}>
+            {SOCIAL.map((item, index) => (
+              <li style={{ marginBlock: 8 }} key={index}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
